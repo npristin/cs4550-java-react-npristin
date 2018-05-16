b33ff6cd70561b4617acd9c12f042c7ec5dbf56d
nicolepristin <nicole.pristin@gmail.com>
2018-05-15 22:40:58 -0400

    Create index.js file and base files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
 ReactDOM.render(
  <h1>Hello, world!</h1>,
  document.getElementById('root')
