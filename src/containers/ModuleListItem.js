b6bf40d6839a166c2832d916a7dcde7c171475c0
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 17:56:15 -0400

    Create a module list item component

---
+++ src/containers/ModuleListItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+export default class ModuleListItem
+  extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li className="list-group-item">
+        {this.props.module.title}
+        <span className="float-right">
+          <i className="fa fa-times" onClick={() =>
+             {this.props.delete(this.props.course.id)}}>
+          </i>
+        </span>
+      </li>
+    );
+  }
+}
