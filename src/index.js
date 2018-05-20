459148333a764c318b8227b63292cb9bd6bdb149
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 17:40:23 -0400

    Render course manager from index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import CourseManager from './containers/CourseManager';
 
 ReactDOM.render(
- <h1>Hello, world!</h1>,
- document.getElementById('root')
+  <div className="container-fluid">
+    <CourseManager/>
+  </div>,
+  document.getElementById('root')
 );
