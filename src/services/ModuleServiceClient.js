0b97acd0fcbc3ac2680f1f57a33775d38c46dfbd
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 17:56:55 -0400

    Create a module service client singleton

---
+++ src/services/ModuleServiceClient.js
@@ -0,0 +1,19 @@
+const MODULE_API_URL =
+  'http://localhost:8080/api/module';
+const MODULE_CID_API_URL =
+  'http://localhost:8080/api/course/CID/module';
+
+let _singleton = Symbol();
+export default class ModuleServiceClient {
+  constructor(singletonToken) {
+    if (_singleton !== singletonToken)
+      throw new Error('Cannot instantiate directly.');
+  }
+
+  static get instance() {
+    if(!this[_singleton])
+      this[_singleton] = new ModuleServiceClient(_singleton);
+    return this[_singleton]
+  }
+
+}
