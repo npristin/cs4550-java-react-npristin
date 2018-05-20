c01a1dac5470a6cca521c643dde455f38ba9bdd4
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 17:49:47 -0400

    Create course editor and its constructor

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,17 @@
+ import React from 'react'
+ import ModuleList from './ModuleList'
+ import ModuleEditor from './ModuleEditor'
+ import CourseServiceClient from '../services/CourseServiceClient'
+
+ export default class CourseEditor
+   extends React.Component {
+
+   constructor(props) {
+     super(props)
+     this.state = {courseId: '', courseTitle: ''};
+     this.selectCourse = this.selectCourse.bind(this);
+     this.courseServiceClient = CourseServiceClient.instance;
+     this.findCourseById = this.findCourseById.bind(this);
+   }
+
+}
