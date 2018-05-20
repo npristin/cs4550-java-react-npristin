d80f9726506096874526cc1a77b4703ef049c5c8
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 17:41:58 -0400

    Add a constructor for course list

---
+++ src/containers/CourseList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import CourseServiceClient from '../services/CourseServiceClient';
+import CourseRow from "../components/CourseRow";
+import '../styles/CourseCard.css'
+
+export default class CourseList extends React.Component {
+ constructor() {
+    super();
+    this.courseServiceClient = CourseServiceClient.instance;
+    this.state = {courses: []};
+    this.titleChanged = this.titleChanged.bind(this);
+    this.createCourse = this.createCourse.bind(this);
+    this.deleteCourse = this.deleteCourse.bind(this);
+ }
+
+)}};
