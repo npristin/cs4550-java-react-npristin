1425e2eb1db80d0b9396cfb2360a66da0ef186b3
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 17:40:55 -0400

    Add course manager and routing for list and editor

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import CourseList from './CourseList';
+import '../styles/CourseCard.css';
+import CourseEditor from './CourseEditor';
+import ModuleEditor from './ModuleEditor';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+export default class CourseManager extends React.Component {
+ render() {
+   return (
+     <Router>
+         <div>
+           <Route path="/courses"
+                  component={CourseList}>
+           </Route>
+           <Route path="/course/:courseId"
+                  component={CourseEditor}>
+           </Route>
+         </div>
+       </Router>
+     )
+   }
+}
