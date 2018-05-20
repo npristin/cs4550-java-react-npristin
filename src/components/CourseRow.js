ced346f5f28d81074ad99ce6433729b2989c6884
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 17:44:57 -0400

    Render a course row

---
+++ src/components/CourseRow.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+class CourseRow extends React.Component {
+   constructor(props) { super(props); }
+   render() {
+       return (
+           <tr>
+            <td>
+                <Link to={`/course/${this.props.course.id}`}>
+                  <i className="fa fa-bars blue-bars"></i>
+                  {this.props.course.title}
+                </Link>
+            </td>
+            <td>
+              me
+            </td>
+            <td>
+              {this.props.course.modified}
+            </td>
+            <td>
+                <i className="fa fa-times" onClick={() =>
+                   {this.props.delete(this.props.course.id)}}>
+                </i>
+            </td>
+           </tr>
+       )
+   }
+}
+export default CourseRow;
