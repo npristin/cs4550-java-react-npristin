212f8a3fcb414f8c77b9facb467fe94468b33e18
nicolepristin <nicole.pristin@gmail.com>
2018-05-20 20:07:42 -0400

    Link to the module on click

--- src/containers/ModuleListItem.js
+++ src/containers/ModuleListItem.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import { Link } from 'react-router-dom'
 
 export default class ModuleListItem
   extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
-      <li className="list-group-item">
+    <Link to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
+      <li className="list-group-item" href="#">
         {this.props.module.title}
         <span className="float-right">
           <i className="fa fa-times" onClick={() =>
@@ -17,6 +20,7 @@ export default class ModuleListItem
           </i>
         </span>
       </li>
+    </Link>
     );
   }
 }
