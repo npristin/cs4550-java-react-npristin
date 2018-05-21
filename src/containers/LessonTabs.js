cd4431353f3b9366197d54367f64f62407106708
nicolepristin <nicole.pristin@gmail.com>
2018-05-21 00:34:54 -0400

    Add constructor for all methods to be used

--- src/containers/LessonTabs.js
+++ src/containers/LessonTabs.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import LessonServiceClient from '../services/LessonServiceClient'
+import LessonTab from '../components/LessonTab'
 
 export default class LessonTabs
   extends React.Component {
-  render() { return(
-    <ul className="nav nav-tabs">
-      <li className="nav-item"><a className="nav-link active"
-                              href="#">Active Tab</a></li>
-      <li className="nav-item"><a className="nav-link"
-                              href="#">Another Tab</a></li>
-    </ul>
-  );}}
+
+  constructor(props) {
+   super(props);
+   console.log(this.props);
+
+   this.state = {
+     lesson: { title: '' },
+     lessons: [],
+     courseId: '',
+     moduleId: ''
+   };
+
+   this.selectModule = this.selectModule.bind(this);
+   this.selectCourse = this.selectCourse.bind(this);
+   this.titleChanged = this.titleChanged.bind(this);
+   this.renderListOfLessons = this.renderListOfLessons.bind(this);
+   this.createLesson = this.createLesson.bind(this);
+   this.deleteLesson = this.deleteLesson.bind(this);
+
+   this.lessonServiceClient = LessonServiceClient.instance;
+ }
+
+}
