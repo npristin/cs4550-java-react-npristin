b139b52efc2b706d9ff8e70a27e5095cc4c5718b
nicolepristin <nicole.pristin@gmail.com>
2018-05-22 14:12:34 -0400

    Refactor to make more neat

--- src/components/LessonTab.js
+++ src/components/LessonTab.js
@@ -4,19 +4,18 @@ import '../styles/LessonTab.css'
 class LessonTab extends React.Component {
 
    render() {
-       return (
-       <div>
-           <li className="nav-item">
-               <a className="nav-link active" href="#">
-                   {this.props.lesson.title}
-                   <i className="fa fa-times fa-2x" onClick={() =>
-                      {this.props.delete(this.props.lesson.id)}}>
-                   </i>
-               </a>
-
-           </li>
-       </div>
-       )
-   }
+    return (
+      <div>
+        <li className="nav-item">
+          <a className="nav-link active" href="#">
+            {this.props.lesson.title}
+            <i className="fa fa-times fa-2x" onClick={() =>
+              {this.props.delete(this.props.lesson.id)}}>
+            </i>
+          </a>
+        </li>
+      </div>
+    )
+  }
 }
 export default LessonTab;
--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -8,12 +8,12 @@ export default class ModuleListItem extends React.Component {
   render() {
     return (
       <li className="list-group-item" href="#">
-      <Link to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
-        {this.props.module.title}
+        <Link to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
+          {this.props.module.title}
         </Link>
         <span className="float-right">
           <i className="fa fa-times" onClick={() =>
-             {this.props.delete(this.props.module.id)}}>
+            {this.props.delete(this.props.module.id)}}>
           </i>
         </span>
       </li>
