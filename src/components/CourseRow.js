15b6f5f71000528fe32adb4ee6863d4d1b51d3ee
nicolepristin <nicole.pristin@gmail.com>
2018-05-22 14:07:24 -0400

    Reformat course row

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -3,26 +3,24 @@ import { Link } from 'react-router-dom'
 
 class CourseRow extends React.Component {
 
-   render() {
-       return (
-           <tr>
-            <td>
-                <Link to={`/course/${this.props.course.id}/module`}>
-                  <i className="fa fa-bars blue-bars"></i>
-                  {this.props.course.title}
-                </Link>
-            </td>
-            <td>
-              me
-            </td>
-            <td>
-              {this.props.course.modified}
-            </td>
-            <td>
-                <i className="fa fa-times" onClick={() =>
-                   {this.props.delete(this.props.course.id)}}>
-                </i>
-            </td>
+    render() {
+        return (
+            <tr>
+                <td>
+                    <Link to={`/course/${this.props.course.id}/module`}>
+                        <i className="fa fa-bars blue-bars"></i>
+                        {this.props.course.title}
+                    </Link>
+                </td>
+                <td>me</td>
+                <td>
+                    {this.props.course.modified}
+                </td>
+                <td>
+                    <i className="fa fa-times" onClick={() =>
+                        {this.props.delete(this.props.course.id)}}>
+                    </i>
+                </td>
            </tr>
        )
    }
