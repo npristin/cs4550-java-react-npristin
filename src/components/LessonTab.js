df37c0f2571f26997a17b5f24d8acaae6d2de32f
nicolepristin <nicole.pristin@gmail.com>
2018-05-21 23:05:37 -0400

    More styling

--- src/components/LessonTab.js
+++ src/components/LessonTab.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import '../styles/LessonTab.css'
 
 class LessonTab extends React.Component {
 
    render() {
        return (
        <div>
-       <li className="nav-item">
-                   <a className="nav-link active" href="#">
-                       {this.props.lesson.title}
-                       <i className="fa fa-times" onClick={() =>
-                          {this.props.delete(this.props.lesson.id)}}>
-                       </i>
-                   </a>
+           <li className="nav-item">
+               <a className="nav-link active" href="#">
+                   {this.props.lesson.title}
+                   <i className="fa fa-times fa-2x" onClick={() =>
+                      {this.props.delete(this.props.lesson.id)}}>
+                   </i>
+               </a>
 
-               </li>
+           </li>
        </div>
        )
    }
