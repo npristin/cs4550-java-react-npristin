3f1f665f0f15971944fc4ca5f14bf8310fbd74d1
nicolepristin <nicole.pristin@gmail.com>
2018-05-23 13:52:04 -0400

    Highlight nav link on click

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import '../styles/ModuleListItem.css'
 
 export default class ModuleListItem extends React.Component {
 
   render() {
+
     return (
+      <NavLink activeClassName="active" to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
       <li className="list-group-item" href="#">
-        <Link to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
           {this.props.module.title}
-        </Link>
         <span className="float-right">
           <i className="fa fa-times" onClick={() =>
             {this.props.delete(this.props.module.id)}}>
           </i>
         </span>
       </li>
+      </NavLink>
     );
   }
 }
