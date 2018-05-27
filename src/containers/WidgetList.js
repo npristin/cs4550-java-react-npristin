75317f1ef138e43b8879b9c074b6ea90f4a5d0df
nicolepristin <nicole.pristin@gmail.com>
2018-05-27 15:35:23 -0400

    Render the widget list

--- src/containers/WidgetList.js
+++ src/containers/WidgetList.js
@@ -9,7 +9,35 @@ class WidgetList extends Component {
     console.log(this.props.lessonId)
 
     this.props.findAllWidgets()
+  }
 
+  render() {
+    return(
+      <div>
+        <h1>Widget List {this.props.widgets.length}</h1>
+
+        <button hidden={this.props.previewMode} onClick={() => {this.props.save(this.props.lessonId, this.props.widgets)}}>
+          Save
+        </button>
+        <button onClick={this.props.preview}>
+          Preview
+        </button>
+        <div>
+          {this.props.lessonId}
+        </div>
+
+        <ul>
+          {this.props.widgets.filter(w => w.lessonId == this.props.lessonId)
+            .map(widget => (
+            <WidgetContainer widget={widget}
+                             preview={this.props.previewMode}
+                             key={widget.id}/>
+          ))}
+        </ul>
+        <button onClick={this.props.addWidget}>Add widget
+        </button>
+      </div>
+    )
   }
 }
 
