5a8d8f1d338b857301d6860d41744bb46dae3996
nicolepristin <nicole.pristin@gmail.com>
2018-05-27 15:36:31 -0400

    Add add widget action

--- src/actions/WidgetActions.js
+++ src/actions/WidgetActions.js
@@ -1,7 +1,6 @@
 import * as constants from "../constants/WidgetConstants"
 
 
-
 export const findAllWidgets = dispatch => {
   fetch('http://localhost:8080/api/widget')
     .then(response => (response.json()))
@@ -9,3 +8,6 @@ export const findAllWidgets = dispatch => {
       type: constants.FIND_ALL_WIDGETS,
       widgets: widgets }))
 }
+export const addWidget = dispatch => (
+  dispatch({type: constants.ADD_WIDGET})
+)
