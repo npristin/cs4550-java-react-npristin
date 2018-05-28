7eb7e0363b4a6613b6933fb0e42c94ef4429af78
nicolepristin <nicole.pristin@gmail.com>
2018-05-27 22:20:49 -0400

    Create heading and render in widget

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -4,16 +4,46 @@ import {DELETE_WIDGET} from "../constants/WidgetConstants"
 import * as actions from '../actions/WidgetActions'
 import '../styles/Widget.css';
 
+const Heading = ({widget, preview, headingTextChanged, headingSizeChanged}) => {
+  let selectElem
+  let inputElem
+  return(
+    <div>
+      <div hidden={preview}>
+        <h2> Heading Widget </h2>
+          <input onChange={() => headingTextChanged(widget.id, inputElem.value)}
+                 value={widget.text}
+                 ref={node => inputElem = node}/>
+          <select onChange={() => headingSizeChanged(widget.id, selectElem.value)}
+                  value={widget.size}
+                  ref={node => selectElem = node}>
+            <option value="1">Heading 1</option>
+            <option value="2">Heading 2</option>
+            <option value="3">Heading 3</option>
+          </select>
+          <h3>Preview</h3>
+      </div>
+      <h1>{widget.text}</h1>
+      {widget.size == 1 && <h1>{widget.text}</h1>}
+      {widget.size == 2 && <h2>{widget.text}</h2>}
+      {widget.size == 3 && <h3>{widget.text}</h3>}
+    </div>
+  )
+}
 
 
+const dispathToPropsMapper = dispatch => ({
+  headingTextChanged: (widgetId, newText) =>
+    actions.headingTextChanged(dispatch, widgetId, newText),
+  headingSizeChanged: (widgetId, newSize) =>
+    actions.headingSizeChanged(dispatch, widgetId, newSize),
+
+})
 const stateToPropsMapper = state => ({
   preview: state.preview
 })
 const HeadingContainer = connect(stateToPropsMapper, dispathToPropsMapper)(Heading)
-const ListContainer = connect(stateToPropsMapper, dispathToPropsMapper)(List)
-const ImageContainer = connect(stateToPropsMapper, dispathToPropsMapper)(Image)
-const LinkContainer = connect(stateToPropsMapper, dispathToPropsMapper)(Link)
-const ParagraphContainer = connect(stateToPropsMapper, dispathToPropsMapper)(Paragraph)
+
 
 const Widget = ({widget, preview, dispatch}) => {
   let selectElement
