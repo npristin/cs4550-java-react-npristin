72f0d220d393ef2b2c5c04b6f86dc9e027ae4a92
nicolepristin <nicole.pristin@gmail.com>
2018-05-27 22:49:19 -0400

    add action for image text changed

--- src/actions/WidgetActions.js
+++ src/actions/WidgetActions.js
@@ -29,6 +29,13 @@ export const listTypeChanged = (dispatch, widgetId, newType) => (
   })
 )
 
+export const imageTextChanged = (dispatch, widgetId, newSrc) => (
+  dispatch({
+    type: constants.IMAGE_SRC_CHANGE,
+    id: widgetId,
+    src: newSrc
+  })
+)
 
 
 export const findAllWidgets = dispatch => {
